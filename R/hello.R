dbe4d5c59f4e967814de0b669b1b6f2aa903c13b
Nathan Freney <nfreney@Nathans.local>
2018-05-24 15:50:41 -0700

    added the sql.read function

--- R/hello.R
+++ R/hello.R
@@ -92,3 +92,11 @@ plotMissing <- function(df, prop=TRUE){
 
   print(p)
 }
+
+#' Loads in a sql file
+#'
+#' @param path String: The path to a sql file
+sql.read <- function(path=getwd()){
+  sql <- readChar(path, file.info(path)$size)
+  return(sql)
+}
