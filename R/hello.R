db11c0f1f1f439285ef8ab05afa1d5a980a54b10
Nathan Freney <nfreney@Nathans.local>
2018-07-20 12:55:08 -0700

    adding two new functions for detecting nas

--- R/hello.R
+++ R/hello.R
@@ -100,3 +100,50 @@ sql.read <- function(path=getwd()){
   sql <- readChar(path, file.info(path)$size)
   return(sql)
 }
+
+#' Finds the indices of all character occurrences in a string. This only works with single
+#' characters.
+#'
+#' @param string String: The character string you want to search
+#' @param item String: The item you would like to find in the string
+#' @return A list of all of the indices where the character can be found
+find_occurrences <- function(string, item){
+
+  # first check to see if the item is in the string at all
+  if(grepl(item, string)==FALSE){
+    print(paste0(item, ' is not in ', string))
+    return()
+  }
+
+  # set up a list to store values
+  list_iter <- 1
+  storage = list()
+
+  # loop through each of the characters in the string to check
+  for(iter in 1:nchar(string)){
+    # if the item is there, add to the list of indices
+    if(string[iter] == item){
+      storage[[list_iter]] <- string[iter]
+      list_iter <- list_iter + 1
+    }
+  }
+
+  return(storage)
+
+}
+
+#' Counts the number of NAs in a column
+#'
+#' @param col String: The column you want to use
+#' @return An integer with the number of NAs in that column
+sumNAs <- function(col) {
+  return(sum(is.na(col)))
+}
+
+#' Counts the number of NAs in all columns in a data frame
+#'
+#' @param df a Dataframe
+#' @return An integer vector containing nas in the column
+detectNAs <- function(df) {
+  return(apply(df, 2, sumNAs))
+}
